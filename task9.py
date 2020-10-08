8c922c2e5d9868cf614540f25603d91dcbde6c7a
Faith-muroa <leagomuroa@gmail.com>
2020-10-08 17:18:54 +0200

    modified task9

--- task9.py
+++ task9.py
@@ -1,12 +1,11 @@
-sum = 0
-threeMultiple = False
-fiveMultiple = False
-
-for i in range(3, 1000):
-    threeMultiple = i % 3 == 0
-    fiveMultiple = i % 5 == 0
-    if (threeMultiple or fiveMultiple):
-        sum += 1
-
-print(sum)
- 
+multiples = []
+b = 1
+while b < 1000:
+    if b % 3 == 0 or b % 5 == 0:
+        multiples.append(b)
+    b = b + 1
+print(multiples)
+sums = 0
+for i in sum_of_multiples:
+    sums = sums + i
+print(sums)
