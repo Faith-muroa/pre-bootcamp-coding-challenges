7721d19537d53e2c952eceb65b59b9f0aa014d44
Faith-muroa <leagomuroa@gmail.com>
2020-10-09 14:36:17 +0200

    updated task4

--- task4.py
+++ task4.py
@@ -1,27 +1,13 @@
 def check3(num1, num2):
-    if num1 == 3 or num2 == 3:
+    if num1 == 3 or num2 == 3 and num1 + num2 == 3:
         return True
-    elif num1 + num2 == 3:
-        return True
+   
     else:
         return False
 
-print(check3(23, 3))
+print(check3(3, 3))
 print(check3(2, 8))
 print(check3(3, 9))
-print(check3(10, 20))
+print(check3(1, 2))
 
-#def check3(num1, num2):
-    #if num1 == 3 or num2 == 3:
-        #return True
-        
-        #A = num1 + num2
-        #A = str(A)
-        #for i in A:
-            #if i == "3":
-                #return True
-            #else:
-                #return False
 
-#print(check3(5, 3))
-#print(check3(1, 2))
